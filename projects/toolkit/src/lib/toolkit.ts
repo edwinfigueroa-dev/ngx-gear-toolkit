f37219832edd80a0a6d7c9b605d24abbfe38367f
Edwin Figueroa <edwinfigueroa.dev@gmail.com>
2026-02-24 10:10:11 -0500

    first commit

---
+++ projects/toolkit/src/lib/toolkit.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-toolkit',
+  imports: [],
+  template: `
+    <p>
+      toolkit works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Toolkit {
+
+}
