1c3bb6a81636c8b8cbb11cda201b05055190e6b3
Edwin Figueroa <edwinfigueroa.dev@gmail.com>
2026-03-04 10:43:13 -0500

    Avances en tabs

--- projects/toolkit-docs/src/app/features/validators/views/number/number.ts
+++ projects/toolkit-docs/src/app/features/validators/views/number/number.ts
@@ -1,10 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { numbersValidator, UiInput } from 'toolkit';
+
 
 @Component({
   selector: 'app-number',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, UiInput],
   templateUrl: './number.html',
   styleUrl: './number.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Number {}
+export class Number {
+  exampleOne = new FormControl(0, [numbersValidator()]);
+}
